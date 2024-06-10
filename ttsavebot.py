730e68470aac82ebf4b57d6d1ccef02b6c040fbf
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-11 00:05:49 +0800

    Update ttsavebot.py

--- ttsavebot.py
+++ ttsavebot.py
@@ -14,6 +14,19 @@ OWNER_LINK = "t.me/sengklek_ais"
 OWNER_DEV = "@sengklek_ais"
 # can edit
 
+# edit text here
+class script(object):
+  ERROR_MSG = """ ❌ Upload error, link salah, video dihapus atau aku tidak bisa menemukannya."""
+  START_MSG = f""" 👋 Halo, saya adalah {BOT_NAME}
+  saya akan membantu anda mengunduh video <b>TikTok</b>.
+  /help - bantuan untuk menggunakan bot.
+  
+  <b> Creator :</b> <a href='{CREATOR_LINK}'>{CREATOR}</a>
+  <b> Developing by :</b> <a href='{OWNER_LINK}'>{OWNER_DEV}</a> """
+  HELP_MSG = """ ❓ Untuk mengunduh video dari <b>TikTok</b>, <b>kirim</b> link ke saya.
+  <b>Link harus dimulai dari:</b>
+  🔗 https://vt.tiktok.com/... """
+# edit text here
 
 import os
 from tiktok_downloader import snaptik
