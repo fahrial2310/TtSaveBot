6171775786bdcc7bb4c65e23630a4c76fecb6d2a
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-11 00:22:56 +0800

    Update ttsavebot.py

--- ttsavebot.py
+++ ttsavebot.py
@@ -54,8 +54,7 @@ def start_command(message):
                              ],
                          ],
                      ),
-                     update.message.reply_text(message_reply_text, reply_markup=reply_markup),
-                  )
+                     update.message.reply_text(message_reply_text, reply_markup=reply_markup))
 
 @bot.message_handler(commands=['help'])
 def help_command(message):
