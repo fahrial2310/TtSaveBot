f2b55712d905f1b8049e25cac912052ac6edbdc5
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-10 22:58:53 +0800

    Update creator_only.py

--- creator_only.py
+++ creator_only.py
@@ -1,7 +1,6 @@
 import os
 
-class config(object):
+class Config(object):
   CREATOR = os.environ.get("CREATOR", "@sengklek_ais")
+  CREATOR_LINK = os.environ.get("CREATOR_LINK", "t.me/sengklek_ais")
   REPO = os.environ.get("REPO", "https://github.com/fahrial2310/TtSaveBot")
-  
-  
