64cf1ef1013cbf280037cf88d574bf7ee4c1f58c
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-10 23:58:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,10 @@ class Config(object):
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "Alvinttsavebot")
 
   OWNER_LINK = os.environ.get("OWNER_LINK", "t.me/sengklek_ais")
+
+  OWNER_DEV = os.environ.get("OWNER_DEV", "@sengklek_ais")
 # for owner development(owner_dev)
 # delete None and change with telegram username, example "@sengklek_ais"
 # Don't forget to use the " sign
-  OWNER_DEV = os.environ.get("OWNER_DEV", "@sengklek_ais")
   
 # add bot token in ttsavebot.py file
