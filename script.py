b347a191ede3e8ddbdb5c198aeb93867cf53ff0b
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-10 22:50:48 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,23 @@
-from config import config
 from creator_only import config
+
+class config(object):
+  BOT_NAME = 'Alvinttsavebot'
+  BOT_USERNAME = 'Alvinttsavebot'
+# for owner development(owner_dev)
+# delete None and change with telegram username, example "@sengklek_ais"
+# Don't forget to use the " sign 
+  OWNER_DEV = '@sengklek_ais'
+  OWNER_LINK = 't.me/sengklek_ais'
+
+# add bot token in ttsavebot.py file
 class script(object):
   ERROR_MSG = """ ❌ Upload error, link salah, video dihapus atau aku tidak bisa menemukannya."""
   START_MSG = """ 👋 Halo, saya adalah {BOT_NAME}
   saya akan membantu anda mengunduh video <b>TikTok</b>.
   /help - bantuan untuk menggunakan bot.
   
-  Creator : {CREATOR}\n Developing by : {OWNER_DEV} """
+  <b> Creator :</b> <a href='{CREATOR_LINK}'>{CREATOR}</a>
+  <b> Developing by :</b> <a href='{OWNER_LINK}'>{OWNER_DEV}</a> """
   HELP_MSG = """ ❓ Untuk mengunduh video dari <b>TikTok</b>, <b>kirim</b> link ke saya.
   <b>Link harus dimulai dari:</b>
   🔗 https://vt.tiktok.com/... """
