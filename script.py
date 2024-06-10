428bfc930311b729a92e3e2df94ce4e6a8587ebc
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-10 20:16:28 +0800

    Update script.py

--- script.py
+++ script.py
@@ -2,11 +2,11 @@ from config import config
 from creator_only import config
 class script(object):
   ERROR_MSG = """ ❌ Upload error, link salah, video dihapus atau aku tidak bisa menemukannya."""
-  START_MSG = """ 👋 Halo, saya adalah {bot_name}
+  START_MSG = """ 👋 Halo, saya adalah {BOT_NAME}
   saya akan membantu anda mengunduh video <b>TikTok</b>.
   /help - bantuan untuk menggunakan bot.
   
-  Creator : {creator}\n Developing by : {owner_dev} """
+  Creator : {CREATOR}\n Developing by : {OWNER_DEV} """
   HELP_MSG = """ ❓ Untuk mengunduh video dari <b>TikTok</b>, <b>kirim</b> link ke saya.
   <b>Link harus dimulai dari:</b>
   🔗 https://vt.tiktok.com/... """
