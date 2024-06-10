8b4578e31966e0d73c31c9dfa2457be2eb49d091
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-10 23:59:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "Alvinttsavebot")
 
   OWNER_LINK = os.environ.get("OWNER_LINK", "t.me/sengklek_ais")
-
+  
   OWNER_DEV = os.environ.get("OWNER_DEV", "@sengklek_ais")
 # for owner development(owner_dev)
 # delete None and change with telegram username, example "@sengklek_ais"
