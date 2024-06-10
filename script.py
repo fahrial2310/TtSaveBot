4891b0001def287947d4e686c8e04825620e5a27
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-10 20:14:58 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
 from ttsavebot import config
 class script(object):
-  error_msg = """ ❌ Upload error, link salah, video dihapus atau aku tidak bisa menemukannya."""
-  start_msg = """ 👋 Halo, saya adalah {bot_name}
+  ERROR_MSG = """ ❌ Upload error, link salah, video dihapus atau aku tidak bisa menemukannya."""
+  START_MSG = """ 👋 Halo, saya adalah {bot_name}
   saya akan membantu anda mengunduh video <b>TikTok</b>.
   /help - bantuan untuk menggunakan bot.
   
   Creator : {creator}\n Developing by : {owner_dev} """
-  help_msg = """ ❓ Untuk mengunduh video dari <b>TikTok</b>, <b>kirim</b> link ke saya.
+  HELP_MSG = """ ❓ Untuk mengunduh video dari <b>TikTok</b>, <b>kirim</b> link ke saya.
   <b>Link harus dimulai dari:</b>
   🔗 https://vt.tiktok.com/... """
