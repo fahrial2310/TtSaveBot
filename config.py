9f6784219abe02f658c0cd83b17738d4f38d0b32
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-10 20:10:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class config(object):
-  bot_name = os.environ.get("bot_name", "Alvinttsavebot")
-  bot_username = os.environ.get("bot_username", "Alvinttsavebot")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN","6713777196:AAH2nhHvxA61iceQfZZccNJ6jkwSGQ2mc-0")
+  BOT_NAME = os.environ.get("bot_name", "Alvinttsavebot")
+  BOT_USERNAME = os.environ.get("bot_username", "Alvinttsavebot")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN","7086014869:AAHJCpJ2vHqbSp32tVFqOkKdtsmE461pIVo")
 # for owner development(owner_dev)
 # delete None and change with telegram username, example "@sengklek_ais"
 # Don't forget to use the " sign 
-  owner_dev = os.environ.get("owner_dev", "@sengklek_ais")
+  OWNER_DEV = os.environ.get("owner_dev", "@sengklek_ais")
