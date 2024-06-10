833773fad9f205abbca0fa2ecd485b33556e66a2
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-10 23:00:55 +0800

    Update ttsavebot.py

--- ttsavebot.py
+++ ttsavebot.py
@@ -4,7 +4,7 @@ from moviepy import editor
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from script import script
 from config import Config
-from creator_only import config
+from creator_only import Config
 
 import telebot
 
