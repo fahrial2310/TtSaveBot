badb1a0e46a005c8b2086575f6545ca04c58c5eb
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-10 19:32:36 +0800

    Update ttsavebot.py

--- ttsavebot.py
+++ ttsavebot.py
@@ -4,6 +4,7 @@ from moviepy import editor
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram import Client, filters
 from script import script
+from config import config
 
 import telebot
 
@@ -11,31 +12,26 @@ TOKEN = "6713777196:AAH2nhHvxA61iceQfZZccNJ6jkwSGQ2mc-0"
 
 bot = telebot.TeleBot(TOKEN)
 
-class config(object):
-  bot_name = os.environ.get("bot_name", "Alvinttsavebot")
-  bot_username = os.environ.get("bot_username", "Alvinttsavebot")
-  creator = os.environ.get("creator", "@sengklek_ais")
-# delete None and change with telegram username, example "@sengklek_ais"
-# Don't forget to use the " sign 
-  owner_dev = os.environ.get("owner_dev", "@sengklek_ais")
-  repo = os.environ.get("repo", "https://github.com/fahrial2310/TtSaveBot")
+@Client.on_message(filters.command(["start"]) & filters.private)
+async def start(client: Client, message: Message):
+    try:
+        await message.reply_text(
+            text=script.start_msg.format(message.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Creator", url={creator}),
+                        InlineKeyboardButton("repo", url={repo}),
+                    ],
+                ]
+            ),
+            reply_to_message_id=message.message_id,
+        )
+    except Exception:
+        pass
 
-@bot.message_handler(commands=['start'])
-def start_command(message):
-    bot.send_message(chat_id=message.chat.id, 
-                    text=('👋 Halo, saya akan membantu anda mengunduh',
-                          ' video <b>TikTok</b>.\n\n Developing by : {owner_dev}',
-                          '/help - bantuan untuk menggunakan bot.'), 
-                    parse_mode='html'),
-                    markup=InlineKeyboardMarkup(
-                      [
-                        [
-                          InlineKeyboardButton("Creator", url={url_creator}),
-                          InlineKeyboardButton("Repo", url={repo}),
-                        ]
-                      ]
-                    )
-                      
+
 @bot.message_handler(commands=['help'])
 def help_command(message):
     bot.send_message(chat_id=message.chat.id, 
