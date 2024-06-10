00801fb10c76e475be73ddcf92dfa5f00967dda2
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-10 20:52:07 +0800

    Update ttsavebot.py

--- ttsavebot.py
+++ ttsavebot.py
@@ -10,7 +10,7 @@ from creator_only import config
 import telebot
 
 # get your bot token on @BotFather
-BOT_TOKEN = "BOT_TOKEN","7086014869:AAHJCpJ2vHqbSp32tVFqOkKdtsmE461pIVo"
+BOT_TOKEN = "7086014869:AAHJCpJ2vHqbSp32tVFqOkKdtsmE461pIVo"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
